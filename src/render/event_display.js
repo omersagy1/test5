f951556b13ed07e7a7190cce31159b75c9ad98d3
omersagy <omersagyemails@gmail.com>
2018-04-28 03:52:39 +0200

    fade

--- src/render/event_display.js
+++ src/render/event_display.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
+const MAX_NUM_LINES = 10;
+
 const EventDisplay = ({events}) => {
   const lines = events.map((e) => e.text).reverse();
-  const line_divs = lines.map(
-    (l) => <LineDisplay line={l} key={l}/>);
+
+  const line_divs = [];
+  for (let [i, l] of lines.entries()) {
+    let opacity = (MAX_NUM_LINES - i) / MAX_NUM_LINES ;
+    line_divs.push(<LineDisplay line={l}
+                                key={l}
+                                opacity={opacity} />);
+  }
 
   return <div>{line_divs}</div>;
 }
 
-const LineDisplay = ({line}) => {
-  return <div>{line}</div>;
+const LineDisplay = ({line, opacity}) => {
+  const style = {
+    opacity: opacity
+  };
+  return <div style={style}>{line}</div>;
 }
 
 export {EventDisplay};
